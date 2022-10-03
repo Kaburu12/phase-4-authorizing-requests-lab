3c6fbc4f75e56501af902418ee7710f9d46392b4
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-03 23:49:16 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
 puts "📃 Seeding data..."
 
-5.times do
-  user = User.create(username: Faker::GreekPhilosophers.name.downcase.gsub(/\W/, '_'))
+# 5.times do
+#   user = User.create(username: Faker::GreekPhilosophers.name.downcase.gsub(/\W/, '_'))
 
-  rand(3..5).times do
-    user.articles.create(
-      title: Faker::Lorem.sentence, 
-      content: Faker::Markdown.sandwich(sentences: rand(3..6), repeat: rand(2..5)),
-      minutes_to_read: rand(3..50),
-      is_member_only: [true, false].sample
-    )
-  end
-end
+#   rand(3..5).times do
+#     user.articles.create(
+#       title: Faker::Lorem.sentence, 
+#       content: Faker::Markdown.sandwich(sentences: rand(3..6), repeat: rand(2..5)),
+#       minutes_to_read: rand(3..50),
+#       is_member_only: [true, false].sample
+#     )
+#   end
+# end
+
+user=User.create(username: "Mike")
 
 puts "✅ Done seeding"
